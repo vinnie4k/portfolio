1466593f7fc4fc6f80a13c83c8ffbeaf40804faf
Vin Bui <vdb23@cornell.edu>
2023-07-02 00:08:54 -0500

    Post2

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import Footer from "./components/Footer";
 import Grabbit from "./pages/work/Grabbit";
 import Home from "./pages/home/Home";
 import NavBar from "./components/NavBar";
-import Post1 from "./pages/posts/Post1";
 import { Route, Routes } from "react-router-dom";
+
+// Blogs
+import Post1 from "./pages/posts/Post1";
+import Post2 from "./pages/posts/Post2";
 
 function App() {
   return (
@@ -12,8 +15,11 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        {/* Blog Posts */}
         <Route path="/blog" element={<Blog />}></Route>
         <Route path="/blog/variables-and-constants" element={<Post1 />}></Route>
+        <Route path="/blog/data-types" element={<Post2 />}></Route>
+        {/* Projects */}
         <Route path="/work/grabbit" element={<Grabbit />}></Route>
       </Routes>
       <Footer />
---
+++ src/pages/posts/Post2.tsx
@@ -0,0 +1,210 @@
+import Callout from "../blog/Callout";
+import CodeBlock from "../blog/CodeBlock";
+import Credits from "../blog/Credits";
+import PostCell from "../../components/PostCell";
+import PostHeader from "./PostHeader";
+import post2_1 from "/src/assets/posts/post2/post2_1.png";
+import post2_2 from "/src/assets/posts/post2/post2_2.png";
+import post2_3 from "/src/assets/posts/post2/post2_3.png";
+import post2_4 from "/src/assets/posts/post2/post2_4.png";
+import post2_5 from "/src/assets/posts/post2/post2_5.png";
+import post2_6 from "/src/assets/posts/post2/post2_6.png";
+
+function Post2() {
+  return (
+    <div className="flex flex-col gap-10 mb-16">
+      {/* Header */}
+      <div>
+        <PostHeader
+          category="Intro to iOS: Swift Basics"
+          date="June 12, 2023"
+          title="Data Types"
+        />
+      </div>
+
+      {/* Intro */}
+      <div className="flex flex-col gap-8">
+        <div>
+          <p className="post-body-mobile">
+            In the variables and constants section above, we assigned a text to
+            a variable. In Swift, this is called a{" "}
+            <span className="font-bold">String</span> and is one of the most
+            important types you will use. However, there are many more types of
+            data that Swift handles. In the example earlier, let’s try to do the
+            following:
+          </p>
+          <CodeBlock codeString={`var instructor\ninstructor = "Vin"`} />
+          <img
+            src={post2_1}
+            alt="Missing type annotation"
+            className="post-code-image-mobile"
+          />
+        </div>
+        <div>
+          <p className="post-body-mobile">
+            There are two ways we can fix this error:
+          </p>
+          <p className="ml-6 post-body-mobile">
+            1. Initialize the variable with a value when we create it.
+          </p>
+          <p className="ml-6 post-body-mobile">
+            2. Tell Swift what data type the variable will hold on later.
+          </p>
+          <p className="post-body-mobile">
+            We’ve already seen (1) earlier, but for (2) we can use{" "}
+            <span className="font-bold">type annotations</span>.
+          </p>
+        </div>
+      </div>
+
+      {/* Type Annotations */}
+      <div className="flex flex-col gap-8">
+        <div>
+          <h2 className="post-heading-mobile">Type Annotations</h2>
+          <CodeBlock
+            codeString={`var instructor: String\ninstructor = "Vin"`}
+          />
+          <img
+            src={post2_2}
+            alt="Using type annotations"
+            className="post-code-image-mobile mb-4"
+          />
+          <Callout
+            icon="ℹ️"
+            text="As convention, we only put a space after the colon and not before!"
+          />
+        </div>
+        <div>
+          <p className="post-body-mobile">
+            The key takeaway here is that Swift is a{" "}
+            <span className="font-bold">statically typed language</span>,
+            meaning that the type of every property, constant and variable that
+            we declare needs to be specified at compile time. This is a good
+            thing because it prevents us from putting anything inside of the
+            “box”. This is known as{" "}
+            <span className="font-bold">type safety</span>. Let’s demonstrate
+            this by introducing a new data type{" "}
+            <span className="font-bold">Int</span> (integer).
+          </p>
+        </div>
+      </div>
+
+      {/* Integer and Type Safety */}
+      <div className="flex flex-col gap-8">
+        <div>
+          <h2 className="post-heading-mobile">Integer and Type Safety</h2>
+          <CodeBlock
+            codeString={`var instructor: String\ninstructor = "Vin"\n\nvar year: Int\nyear = 2025`}
+          />
+          <img
+            src={post2_3}
+            alt="Integer and type safety valid"
+            className="post-code-image-mobile"
+          />
+        </div>
+        <div>
+          <p className="post-body-mobile">
+            Everything works fine, but what if we were to swap the values of{" "}
+            <span className="post-code">instructor</span> and{" "}
+            <span className="post-code">year</span>?
+          </p>
+          <img
+            src={post2_4}
+            alt="Integer and type safety invalid"
+            className="post-code-image-mobile"
+          />
+        </div>
+        <p className="post-body-mobile">
+          Our code no longer compiles because we tried assigning a value whose
+          type does not match the type of the variable at the time it was
+          created.
+        </p>
+      </div>
+
+      {/* Float and Double */}
+      <div className="flex flex-col gap-8">
+        <div>
+          <h2 className="post-heading-mobile">Float and Double</h2>
+          <p className="post-body-mobile">
+            You can store decimal numbers by using a{" "}
+            <span className="font-bold">Float</span> and{" "}
+            <span className="font-bold">Double</span>. The difference between
+            these two is that a Double has twice the accuracy of a Float, and
+            hence takes up more storage.
+          </p>
+          <img
+            src={post2_5}
+            alt="Float vs Double"
+            className="post-code-image-mobile"
+          />
+        </div>
+      </div>
+
+      {/* Boolean */}
+      <div className="flex flex-col gap-8">
+        <div>
+          <h2 className="post-heading-mobile">Boolean</h2>
+          <p className="post-body-mobile">
+            A <span className="font-bold">Bool</span> (boolean) in Swift is a
+            data type that can hold one of two values:{" "}
+            <span className="post-code">true</span> or{" "}
+            <span className="post-code">false</span>.
+          </p>
+          <img
+            src={post2_6}
+            alt="Using booleans in Swift"
+            className="post-code-image-mobile"
+          />
+        </div>
+      </div>
+
+      {/* Type Inferencing */}
+      <div className="flex flex-col gap-8">
+        <div>
+          <h2 className="post-heading-mobile">Type Inferencing</h2>
+          <p className="post-body-mobile">
+            Earlier when we assigned an initial value to a variable,
+          </p>
+          <CodeBlock codeString={`var instructor = "Vin"`} />
+          <p className="post-body-mobile mt-4">
+            Swift automatically <span className="italic">infers</span> what data
+            type the variable will hold. This is known as{" "}
+            <span className="font-bold">type inferencing</span>. You could also
+            specify a data type and provide an initial value at the same time:
+          </p>
+          <CodeBlock codeString={`var instructor: String = "Vin"`} />
+        </div>
+        <p className="post-body-mobile">
+          Most of the time, we will not be using type annotations and prefer
+          having Swift infer our types. However, there are times when type
+          annotation should be used. This will come with practice and from
+          seeing how we write our code.
+        </p>
+      </div>
+
+      {/* Credits */}
+      <Credits />
+
+      {/* See More */}
+      <div className="flex flex-col gap-8">
+        <h2 className="text-2xl font-bold text-center">
+          More on Intro to iOS: Swift Basics
+        </h2>
+        <PostCell
+          date="June 1"
+          title="Variables and Constants"
+          body="In almost any program that we create, we will need to store data at some point. In Swift, you can store data in two ways: variables and constants."
+          destination="/blog/variables-and-constants"
+        />
+        <PostCell
+          date="June 24"
+          title="Operators"
+          body="You have probably seen the four basic math operations in elementary school: addition, subtraction, multiplication, and division. In Swift, you can use operators to perform these operations."
+          destination="/blog/operators"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Post2;
